76dca7d839c4d31b2492f364741b4772ddb15824
Shivanshi Tiwari <shivanshitiwari381@gmail.com>
2024-01-04 19:56:02 +0530

    modified

--- script.js
+++ script.js
@@ -1,53 +1,46 @@
 const accessKey = "k-ISjqmN_vwNQyROZz7XigkmAKXNvv1yW2OfGY0kXcI";
+// Now import all the important elements from the HTML file
 const formEl = document.querySelector("form");
 const inputEl = document.getElementById("search-input");
+// for classes use . before the name of the class
 const searchResults = document.querySelector(".search-results");
 const showMore = document.getElementById("show-more-button");
 
+let inputData = "";
 let page = 1;
 
+// $ is used for dynamic data
+// async is used for response and fetch
+
 async function searchImages() {
-  try {
-    const inputData = inputEl.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
-    const response = await fetch(url);
-
-    if (!response.ok) {
-      throw new Error(`Error: ${response.status} - ${response.statusText}`);
-    }
-
-    const data = await response.json();
-    const results = data.results;
-
-    if (page === 1) {
-      searchResults.innerHTML = "";
-    }
-
-    results.forEach((result) => {
-      const imageWrapper = document.createElement("div");
-      imageWrapper.classList.add("search-result");
-
-      const image = document.createElement("img");
-      image.src = result.urls.small;
-      image.alt = result.alt_description;
-
-      const imageLink = document.createElement("a");
-      imageLink.href = result.links.html;
-      imageLink.target = "_blank";
-      imageLink.innerText = result.alt_description;
-
-      imageWrapper.appendChild(image);
-      imageWrapper.appendChild(imageLink);
-      searchResults.appendChild(imageWrapper);
-    });
-
-    page++;
-    if (page > 1) {
-      showMore.style.display = "block";
-    }
-  } catch (error) {
-    console.error("Error:", error);
-    // Handle errors appropriately
+  inputData = inputEl.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  const results = data.results;
+  if (page === 1) {
+    searchResults.innerHTML = "";
+  }
+  results.map((result) => {
+    const imageWrapper = document.createElement("div");
+    imageWrapper.classList.add("search-result");
+  
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    image.alt = result.alt_description;
+  
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+    imageLink.innerText = result.alt_description;
+  
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(imageLink);
+    searchResults.appendChild(imageWrapper);
+  });
+  page++;
+  if (page > 1) {
+    showMore.style.display = "block";
   }
 }
 
